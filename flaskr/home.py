c0863a0d70c3e0652d7d22cfbec9b7ce6c836e15
topshager <rem28777@gmail.com>
2024-11-16 23:54:17 +0200

    Update home.py

--- flaskr/home.py
+++ flaskr/home.py
@@ -6,4 +6,8 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('home', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db
