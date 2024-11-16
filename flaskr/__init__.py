4f14c2fd79e59be8c6256e177728eade9525ecb4
topshager <rem28777@gmail.com>
2024-11-16 23:54:13 +0200

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,6 +28,7 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     from.import home
-    app.register_blueprint(auth.bp)
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
