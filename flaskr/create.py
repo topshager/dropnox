b53fea61e34215f59ae6c397b270d944556506b3
topshager <rem28777@gmail.com>
2024-11-22 23:03:50 +0200

    file upload to database complete

--- flaskr/create.py
+++ flaskr/create.py
@@ -1,3 +1,6 @@
+
+
+from flask import Flask ,jsonify
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -8,6 +11,22 @@ from flaskr.db import get_db
 import os
 bp = Blueprint('create', __name__)
 
-@bp.route('/create')
+@bp.route('/create',methods=('GET','POST'))
 def createfolder():
+    if request.method == 'POST':
+        foldername = request.form['folder-name']
+        db = get_db()
+        error = None
+
+        if not foldername:
+            error = 'File name cannot be blank'
+        if error  is None:
+            try:
+                db.execute(
+                    " INSERT INTO folders (name) VALUES (?)",
+                    (foldername,),
+                )
+            except db.IntegrityError:
+                error = f"file {foldername} is already created"
+        flash(error)
     return render_template('homepage/createfolder.html')
