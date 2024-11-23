109fde3d5d1c730de99c0ab8f94bd2c45063cc7b
topshager <rem28777@gmail.com>
2024-11-23 20:15:12 +0200

    added user foreign key to files and folders tables

--- flaskr/create.py
+++ flaskr/create.py
@@ -9,37 +9,38 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 import os
 bp = Blueprint('create', __name__)
-@bp.route('/create',methods=('GET','POST'))
+
+
+@bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def createfolder():
-    db = get_db
+    db = get_db()  # Call the function to get the database connection
     error = None
-    folders = []
 
     if request.method == 'POST':
-        foldername = request.form['folder-name']
-
-
+        foldername = request.form.get('folder-name')  # Safely get the folder name
         if not foldername:
-            error = 'File name cannot be blank'
+            error = 'Folder name cannot be blank'
         else:
             try:
+                # Insert folder name with the username of the logged-in user
                 db.execute(
-                    " INSERT INTO folders (name) VALUES (?)",
-                    (foldername,),
+                    "INSERT INTO folders (name ) VALUES (?)",
+                    (foldername)
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"file {foldername} is already created"
-
+                error = f"Folder '{foldername}' already exists"
 
         if error:
             flash(error)
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('create.createfolder'))  # Stay on the same page after creation
+
+    # Fetch all folders for the current user
     folders = db.execute(
-        "SELECT * FROM folders WHERE username = ?",
-        (g.user['username'],)  # Assuming g.user['username'] contains the username
+        "SELECT * FROM users WHERE username = ?",
+        (g.user['username'],)
     ).fetchall()
 
-    return render_template('homepage/createfolder.html')
+    return render_template('homepage/createfolder.html', folders=folders)
