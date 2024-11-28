3ae6b40bdd415d722dcc1873c3d753ee6bd2bb2d
topshager <rem28777@gmail.com>
2024-11-28 10:34:22 +0200

    creation of upload route and creation of upload.html

--- flaskr/upload.py
+++ flaskr/upload.py
@@ -10,5 +10,34 @@ from flaskr.db import get_db
 
 bp = Blueprint('upload', __name__)
 
-@login_required
 
+UPLOAD_FOLDER = 'uplaods'
+ALLOWED_EXTENSIONS = {'png','jpeg','gif','pdf','txt'}
+
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+
+def allowed_files(filenaem):
+    return '.' in filenaem and filenaem.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/upload',methods=['POST'])
+def uploade():
+
+        return render_template('homepage/upload.html')
+
+
+
+
+#   if 'file' not in request.files:
+#        flash('no file part')
+#        return redirect('home.html')
+#    file = request.files['file']
+#    if file.filename == '':
+#        flash('No selected file')
+#        return redirect('home.html')
+#    if file and allowed_files(file.filename):
+#        filename = secure_filename(file.filename)
+#        file.save(os.path.join(bp.config['UPLOAD_FOLDER'], filename))
+#        flash('File uploaded succcessfully!')
+#    flash('Invalid file type')
+#    return redirect('hompage/home.html')
+#
